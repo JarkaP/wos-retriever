e958ef29b4e799ad2485466f3e29657541f9bac7
JarkaP <jarka.p@gmail.com>
2019-01-30 17:52:36 +0100

    change layout

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,24 +1,22 @@
 <template>
-  <div id="app" class="container-fluid px-0">
-      <div class="row">
-          <div class="col">
-              <Navigation class="mb-5"></Navigation>
-          </div>
-      </div>
-      <div class="row justify-content-md-center">
-          <div class="col-md-auto mx-3">
-              <router-view></router-view>
-          </div>
-      </div>
+    <div id="app">
+        <Navigation class="mb-5"></Navigation>
 
-  </div>
+        <div class="container-fluid">
+            <div class="row justify-content-md-center">
+                <div class="col-md-auto mx-3">
+                    <router-view></router-view>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-  import Navigation from './components/Navigation';
+import Navigation from './components/Navigation';
 
-  export default {
-      components: { Navigation },
-      name: 'wos-retriever'
-  }
+export default {
+    components: { Navigation },
+    name: 'wos-retriever'
+};
 </script>
