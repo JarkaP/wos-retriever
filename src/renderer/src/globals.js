c95817c7f512b468bc42e19e5a9f4218870c6877
JarkaP <jarka.p@gmail.com>
2019-01-30 15:11:47 +0100

    register global variables

---
+++ src/renderer/src/globals.js
@@ -0,0 +1,64 @@
+/* global require module */
+
+const homedir = require('os').homedir();
+
+const searchUrl =
+    'http://search.webofknowledge.com/esti/wokmws/ws/WokSearch?wsdl';
+const authUrl =
+    'http://search.webofknowledge.com/esti/wokmws/ws/WOKMWSAuthenticate?wsdl';
+
+const countLimit = 100;
+const idLimit = 15000;
+const timeLimit = 1200;
+const wosEditions = [
+    {
+        collection: 'WOS',
+        edition: 'SCI'
+    },
+    {
+        collection: 'WOS',
+        edition: 'SSCI'
+    },
+    {
+        collection: 'WOS',
+        edition: 'AHCI'
+    },
+    {
+        collection: 'WOS',
+        edition: 'ISTP'
+    },
+    {
+        collection: 'WOS',
+        edition: 'ISSHP'
+    },
+    {
+        collection: 'WOS',
+        edition: 'IC'
+    },
+    {
+        collection: 'WOS',
+        edition: 'CCR'
+    },
+    {
+        collection: 'WOS',
+        edition: 'BSCI'
+    },
+    {
+        collection: 'WOS',
+        edition: 'BHCI'
+    },
+    {
+        collection: 'WOS',
+        edition: 'ESCI'
+    }
+];
+
+module.exports = {
+    searchUrl,
+    authUrl,
+    countLimit,
+    timeLimit,
+    idLimit,
+    wosEditions,
+    homedir
+};
