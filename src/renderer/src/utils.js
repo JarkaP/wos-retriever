f0b9aaeaabe89a0562ac04af28e1dc51db8e1477
Jarka <jarka.p@gmail.com>
2019-01-30 22:13:28 +0100

    Create utils.js

---
+++ src/renderer/src/utils.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import flatten from '@flatten/array';
+import parse from 'csv-parse/lib/sync';
+
+export default function getRecordsID(dataFile) {
+    let file = fs.readFileSync(dataFile, 'utf8');
+
+    let wosIDs = parse(file, {
+        skip_empty_lines: true
+    });
+
+    return flatten(wosIDs);
+}
