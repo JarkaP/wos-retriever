27533a3636e54ab1b6b13f59ed22f02fcb4f21f9
JarkaP <jarka.p@gmail.com>
2019-01-31 11:58:58 +0100

    remove redundant components

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,29 +1,9 @@
 <template>
-    <div style="width:320px">
-        <b-list-group>
-            <b-list-group-item href="#/new-project-page">New project</b-list-group-item>
-            <b-list-group-item href="#/project-page">Projects</b-list-group-item>
-            <b-list-group-item href="#/login-page">Authorization link</b-list-group-item>
-        </b-list-group>
-    </div>
+    <div style="width:320px"></div>
 </template>
 
 <script>
 export default {
-    name: 'landing-page',
-}
-/*
-import SystemInformation from './LandingPage/SystemInformation'
-
-export default {
-name: 'landing-page',
-components: { SystemInformation },
-methods: {
-open (link) {
-this.$electron.shell.openExternal(link)
-}
-}
-}
-
-*/
+    name: 'landing-page'
+};
 </script>
